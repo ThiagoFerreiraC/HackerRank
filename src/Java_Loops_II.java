8dde582e19491ab6bdd95c1fe5b496e45f9813f7
Thiago F. Carneiro <thiago.fcarneiro@sinqia.com.br>
2023-04-24 16:10:44 -0300

    Initial commit

---
+++ src/Java_Loops_II.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Java_Loops_II {
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int expoente =0;
+
+            for(int j=0; j<n; j++) {
+                expoente += (int) Math.pow(2, j)*b;
+                int termo = a + expoente;
+                System.out.print(termo + " " );
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
